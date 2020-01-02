d4ec6e2c46b2aea1913d69edd2a82f6fbea4973d
admin <admin-at-803553529221>
2020-01-02 17:23:44 +0530

    added security added login page

--- auth-login-app/src/app/auth/auth.service.ts
+++ auth-login-app/src/app/auth/auth.service.ts
@@ -11,29 +11,59 @@ export class AuthService {
     domain: 'single-page-app-io.auth0.com',
     redirectUri: 'http://localhost:4200',
     responseType: 'token id_token',
-    audience: 'https://single-page-app-io.auth0.com/userinfo'
+    audience: 'https://single-page-app-io.auth0.com/userinfo',
+    scope: 'openid profile'
   });
+
+  profile: any;
 
   constructor() { }
 
-  login() {
+  login(): void {
     this.auth0.authorize();
   }
 
-  handleAuthentication() {
+  handleAuthentication(): void {
     this.auth0.parseHash((error, authResult) => {
       if (authResult && authResult.accessToken && authResult.idToken) {
         this.setSession(authResult);
+        this.fetchProfile();
+        window.location.hash = '';
       } else if (error) {
         console.log(error);
       }
     });
   }
 
-  setSession(authResult){
+  setSession(authResult): void {
     const expiredAt = JSON.stringify((authResult.expiresIn * 1000) + new Date().getTime());
     localStorage.setItem('access_token', authResult.accessToken);
     localStorage.setItem('id_token', authResult.idToken);
     localStorage.setItem('expires_at', expiredAt);
+  }
+
+  isAuthenticated(): boolean {
+    const expiresAt = JSON.parse(localStorage.getItem('expires_at'));
+    return new Date().getTime() < expiresAt;
+  }
+
+  logout(): void {
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('id_token');
+    localStorage.removeItem('expires_at');
+  }
+
+  fetchProfile(): void {
+    const accessToken = localStorage.getItem('access_token');
+    if (!accessToken) {
+      throw new Error('Access token requried for fetching a profile')
+    }
+    this.auth0.client.userInfo(accessToken, (err, profile) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      this.profile = profile;
+    });
   }
 }
